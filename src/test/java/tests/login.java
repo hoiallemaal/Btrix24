a68a7a70e5e4aad71c84b46ffa738a13777cf11e
Sukru <skrcakmaktepe@gmail.com>
2019-12-02 21:19:50 +0100

    updated 2 files

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -5,6 +5,6 @@ import sun.jvm.hotspot.HelloWorld;
 public class login {
 
  public static void main(String[] args) {
-     System.out.println("HelloWorld");
+
 }
 }
--- src/test/java/tests/logout.java
+++ src/test/java/tests/logout.java
@@ -2,6 +2,6 @@ package tests;
 
 public class logout {
     public static void main(String[] args) {
-
+        System.out.println("hello");
     }
 }
