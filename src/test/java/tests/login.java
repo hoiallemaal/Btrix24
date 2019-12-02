0df2e3992b8873ed37240f7e57edb548931d843d
Sukru <skrcakmaktepe@gmail.com>
2019-12-02 19:41:05 +0100

    added logout file

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -1,6 +1,10 @@
 package tests;
 
+import sun.jvm.hotspot.HelloWorld;
+
 public class login {
 
-
+ public static void main(String[] args) {
+     System.out.println("HelloWorld");
+}
 }
---
+++ src/test/java/tests/logout.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class logout {
+    public static void main(String[] args) {
+
+    }
+}
