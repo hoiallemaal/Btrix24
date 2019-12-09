515e652a9945c0c1c99bec67aa10c547b93737ab
Sukru <skrcakmaktepe@gmail.com>
2019-12-09 19:15:33 +0100

    add void method

--- src/test/java/tests/Task.java
+++ src/test/java/tests/Task.java
@@ -1,15 +1,14 @@
 package tests;
 
 public class Task {
-
-  
-  public static void main(String [] args){
+ public static void main(String [] args){
   System.out.println("Hello World");
       System.out.println("Hello World");
-
      System.out.println("Hello Everyone");
     //this line is added from githup. see you in intelijej
-  
+   }
+   public void hi(){
+      System.out.println("Hi from IntelliJ");
   }
 
 }
