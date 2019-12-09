a2716666a0a84338e0673eef266230218099ff78
hoiallemaal <47101585+hoiallemaal@users.noreply.github.com>
2019-12-09 18:37:53 +0100

    updated task class

--- src/test/java/tests/Task.java
+++ src/test/java/tests/Task.java
@@ -1,4 +1,11 @@
 package tests;
 
 public class Task {
+  
+  public static void main(String [] args){
+  System.out.println("Hello World");
+    
+    //this line is added from githup. see you in intelijej
+  
+  }
 }
